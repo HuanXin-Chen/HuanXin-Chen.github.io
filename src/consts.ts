1a58a7af0a3d85e2ad532373336da48914fc01b9
H_X <111850224+HuanXin-Chen@users.noreply.github.com>
2023-08-27 15:52:27 +0800

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,47 @@
+export const SITE = {
+	title: 'H_X‘s blog👨‍💻 ',
+	description: 'Your website description.',
+	defaultLanguage: 'en-us',
+} as const
+
+export const OPEN_GRAPH = {
+	image: {
+		src: '',
+		alt:
+			'astro logo on a starry expanse of space,' +
+			' with a purple saturn-like planet floating in the right foreground',
+	},
+	twitter: 'astrodotbuild',
+}
+
+export const KNOWN_LANGUAGES = {
+	English: 'en',
+} as const
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES)
+
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`
+
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
+}
+
+export type Sidebar = Record<
+	(typeof KNOWN_LANGUAGE_CODES)[number],
+	Record<string, { text: string; link: string }[]>
+>
+export const SIDEBAR: Sidebar = {
+	en: {
+		'🌱WELCOME': [
+			{ text: 'Introduction', link: 'en/introduction' },
+		],
+		'POST2023💬': [
+			{text:'反射实现DTO速转VO',link:'en/page-2'},
+			{text:'SpringBoot3极简上手指南', link:'en/page-1'},
+		],
+	},
+}
---
+++ src/content/docs/en/introduction.md
@@ -0,0 +1,18 @@
+---
+title: "Hello! I'm H_X"
+description: "Docs intro"
+---
+
+#### 🤓 一位平平无奇的大学生，时刻努力中ing
+
+##### 持续学习中..
+
+- 🥳 My Github : [HuanXin-Chen](https://github.com/HuanXin-Chen)
+- 🌱 I’m currently learning Java , AICG , Distributed.
+- 🎢 Language proficiency rankings : C++>C>Java>Python>Rust>JS.
+- 📫 WeChat public account : 阿新的杂记.
+- ⚡ Fun fact : Photography , Popping , Guitar.
+- 💬 Ask me about anything about CS.
+- ❤️ Love anything that's high-performance and easy to use.
+
+Good luck out there, my friend. 🥳
