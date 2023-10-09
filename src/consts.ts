c20e202975366242d22c39372850811045454b7d
H_X <111850224+HuanXin-Chen@users.noreply.github.com>
2023-10-09 17:10:13 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -40,6 +40,7 @@ export const SIDEBAR: Sidebar = {
 			{ text: 'Introduction', link: 'en/introduction' },
 		],
 		'POST2023💬': [
+			{text:'一文自顶向下串起MySQL',link:'en/page-5'},
 			{text:'分布式ID中的SnowFlake',link:'en/page-4'},
 			{text:'反射实现DTO速转VO',link:'en/page-3'},
 			{text:'SpringBoot3极简上手指南', link:'en/page-2'},
